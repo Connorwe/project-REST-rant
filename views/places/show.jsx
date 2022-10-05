fb23df2408705a1c6fbac42c4608b34ebce6bd33
Connor <connor5741213@gmail.com>
2022-10-05 10:54:48 -0400

    Delete functioniality implemented and package.json reinstalled

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,21 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{data.place.name}</h1>  
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                Edit
+            </a>  
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <button type="submit" className="btn btn-danger">
+                  Delete
+                </button>
+            </form>   
+            <h2>{data.place.city}</h2>
+            <img src={data.place.pic} alt={data.place.name}/>
           </main>
         </Def>
     )
